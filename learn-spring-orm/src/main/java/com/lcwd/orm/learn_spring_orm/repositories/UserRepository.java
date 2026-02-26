3b6e773cbd05598530afc9ee3c6a8a769b3cdd4d
CHITRANSHU YADAV <139374527+Chitranshu2411@users.noreply.github.com>
2026-02-26 23:28:59 +0530

    Delete learn-spring-orm/src/main/java/com/lcwd/orm/learn_spring_orm/repositories directory

--- learn-spring-orm/src/main/java/com/lcwd/orm/learn_spring_orm/repositories/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.lcwd.orm.learn_spring_orm.repositories;
-
-import com.lcwd.orm.learn_spring_orm.entities.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User,Integer>
-{
-
-}
